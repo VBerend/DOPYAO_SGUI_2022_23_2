077f3f5d5c21968d5ed4a3c3ffc25891c64d1f67
VBerend <berend.vajk@gmail.com>
2022-03-23 21:59:29 +0100

    dbcontext

--- DOPYAO_ADT_2021_22_2/DOPYAO_ADT_2021_22_2.Data/ShelterDbContext.cs
+++ DOPYAO_ADT_2021_22_2/DOPYAO_ADT_2021_22_2.Data/ShelterDbContext.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using DOPYAO_ADT_2021_22_2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DOPYAO_ADT_2021_22_2.Data
-{
-	public class ShelterDbContext
+{public class ShelterDbContext
 	{
+		public ShelterDbContext(DbContextOptions<ShelterDbContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public virtual DbSet<Animal> Animals { get; set; }
+		public virtual DbSet<Adopter> Adopters { get; set; }
+		public virtual DbSet<Shelter> Shelters { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder builder)
+		{
+			if (builder != null && !builder.IsConfigured)
+			{
+				builder.UseLazyLoadingProxies().UseSqlServer(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\Database.mdf; Integrated Security = True; ");
+			}
+		}
 	}
 }
+
