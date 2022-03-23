b20b5ed9f477ff23a37268ffdc7b3cba6b15ca31
VBerend <berend.vajk@gmail.com>
2022-03-23 21:58:13 +0100

    animals, and shelters

--- DOPYAO_ADT_2021_22_2/DOPYAO_ADT_2021_22_2.Models/Shelter.cs
+++ DOPYAO_ADT_2021_22_2/DOPYAO_ADT_2021_22_2.Models/Shelter.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace DOPYAO_ADT_2021_22_2.Models
 {
-	class Shelter
+	[Table("shelter")]
+	public class Shelter
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[MaxLength(100)]
+		[Required]
+		public string ShelterName { get; set; }
+
+		[MaxLength(100)]
+		[Required]
+		public string City { get; set; }
+
+		[MaxLength(100)]
+		[Required]
+		public string Address { get; set; }
+
+		[NotMapped]
+		[JsonIgnore]
+		public virtual ICollection<Animal> Animals { get; }
+
+		public Shelter()
+		{
+			Animals = new HashSet<Animal>();
+		}
 	}
 }
