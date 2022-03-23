1cbff6e2a9adaf81709f8e2c7b1a354cf80219d4
VBerend <berend.vajk@gmail.com>
2022-03-23 21:54:56 +0100

    renamed classes and stuff

---
+++ DOPYAO_ADT_2021_22_2/DOPYAO_ADT_2021_22_2.Models/Shelter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOPYAO_ADT_2021_22_2.Models
+{
+	class Shelter
+	{
+	}
+}
