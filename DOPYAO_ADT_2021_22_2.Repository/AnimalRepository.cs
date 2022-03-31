fa82eaca08906e4eb46f3e44102e077da3c9698f
VBerend <berend.vajk@gmail.com>
2022-03-31 21:12:28 +0200

    Animal repo finish

--- DOPYAO_ADT_2021_22_2.Repository/AnimalRepository.cs
+++ DOPYAO_ADT_2021_22_2.Repository/AnimalRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DOPYAO_ADT_2021_22_2.Data;
+using DOPYAO_ADT_2021_22_2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace DOPYAO_ADT_2021_22_2.Repository
 {
-	class AnimalRepository
+	class AnimalRepository : Repository<Animal>, IAnimalRepository
 	{
+		public AnimalRepository(ShelterDbContext context) : base(context)
+		{
+		}
+
+		public void ChangeName(int id, string newName)
+		{
+			var animal = this.GimmeOne(id);
+			if (animal == null)
+			{
+				throw new InvalidOperationException("Record not found");
+			}
+			animal.Name = newName;
+			this.Context.SaveChanges();
+		}
+
+		public override void Delete(int id)
+		{
+			Animal obj = this.GimmeOne(id);
+			this.Context.Set<Animal>().Remove(obj);
+			this.Context.SaveChanges();
+		}
+
+		public override Animal GimmeOne(int id)
+		{
+			return this.GimmeAll().SingleOrDefault(x => x.Id == id);
+		}
+
+		public override void Insert(Animal entity)
+		{
+			this.Context.Set<Animal>().Add(entity);
+			this.Context.SaveChanges();
+		}
 	}
 }
---
+++ DOPYAO_ADT_2021_22_2.Repository/IAnimalRepository.cs
@@ -0,0 +1,9 @@
+﻿using DOPYAO_ADT_2021_22_2.Models;
+
+namespace DOPYAO_ADT_2021_22_2.Repository
+{
+	public interface IAnimalRepository : IRepository<Animal>
+	{
+		void ChangeName(int id, string newName);
+	}
+}
