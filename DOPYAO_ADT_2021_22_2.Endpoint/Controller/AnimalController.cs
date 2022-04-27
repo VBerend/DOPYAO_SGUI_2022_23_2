2a6f54bfbf37f9aa2d6c3e31fc2d2cce0a4885ac
VBerend <berend.vajk@gmail.com>
2022-04-27 23:43:44 +0200

    shelter and animal controller

--- DOPYAO_ADT_2021_22_2.Endpoint/Controller/AnimalController.cs
+++ DOPYAO_ADT_2021_22_2.Endpoint/Controller/AnimalController.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using DOPYAO_ADT_2021_22_2.Logic;
+using DOPYAO_ADT_2021_22_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DOPYAO_ADT_2021_22_2.Endpoint.Controller
 {
-	public class AnimalController
+	[Route("[controller]")]
+	[ApiController]
+	public class AnimalController : ControllerBase
 	{
-	}
+        IAnimalLogic animalLogic;
+
+        public AnimalController(IAnimalLogic animalLogic)
+        {
+            this.animalLogic = animalLogic;
+        }
+
+        [Route("GetAllAnimals")]
+        [HttpGet]
+        public IQueryable<Animal> GetAllAnimals()
+        {
+            return animalLogic.GetAllAnimals().AsQueryable();
+        }
+
+        [Route("GetOneAnimal/{id}")]
+        [HttpGet]
+        public Animal GetOneAnimal(int id)
+        {
+            return animalLogic.GetOneAnimal(id);
+        }
+
+        [Route("InsertNewAnimal/{name}/{gender}/{specie}/{bodysize}/{age}")]
+        [HttpPost]
+        public Animal InsertNewAnimal([FromBody] string name, string gender, string specie, string bodysize, int age)
+        {
+            return animalLogic.InsertNewAnimal(name, gender, specie, bodysize, age);
+        }
+
+        [Route("DeleteAnimal/{id}")]
+        [HttpDelete]
+        public void DeleteAnimal(int id)
+        {
+            animalLogic.DeleteAnimal(id);
+        }
+
+        [Route("ChangeCustomerName/{id}/{newName}")]
+        [HttpPut]
+        public void ChangeAnimalName(int id, string newName)
+        {
+            animalLogic.ChangeAnimalName(id, newName);
+        }
+    }
 }
--- DOPYAO_ADT_2021_22_2.Endpoint/Controller/ShelterController.cs
+++ DOPYAO_ADT_2021_22_2.Endpoint/Controller/ShelterController.cs
@@ -1,11 +1,78 @@
-﻿using System;
+﻿using DOPYAO_ADT_2021_22_2.Logic;
+using DOPYAO_ADT_2021_22_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DOPYAO_ADT_2021_22_2.Endpoint.Controller
 {
-	public class ShelterController
+    [Route("[controller]")]
+    [ApiController]
+    public class ShelterController : ControllerBase
 	{
-	}
+        IShelterLogic shelterLogic;
+
+        public ShelterController(IShelterLogic shelterLogic)
+        {
+            this.shelterLogic = shelterLogic;
+        }
+
+        [Route("GetAllShelters")]
+        [HttpGet]
+        public IQueryable<Shelter> GetAllShelters()
+        {
+            return shelterLogic.GetAllShelters().AsQueryable();
+        }
+
+        [Route("GetOneShelter/{id}")]
+        [HttpGet]
+        public Shelter GetOneShelter(int id)
+        {
+            return shelterLogic.GetOneShelter(id);
+        }
+
+        [Route("InsertNewShelter/{ShelterName}/{City}/{Address}")]
+        [HttpPost]
+        public Shelter InsertNewShelter([FromBody] string sheltername, string city, string address)
+        {
+            return shelterLogic.InsertNewShelter(sheltername, city, address);
+        }
+
+        [Route("DeleteShelter/{id}")]
+        [HttpDelete]
+        public void DeleteShelter(int id)
+        {
+            shelterLogic.DeleteShelter(id);
+        }
+
+        [Route("ChangeShelterAddress/{id}/{newCity}/{newAddress}")]
+        [HttpPut]
+        public void ChangeShelterAddress(int id, string newCity, string newAddress)
+        {
+            shelterLogic.ChangeShelterAddress(id, newCity, newAddress);
+        }
+
+        [Route("animalfromshelter")]
+        [HttpGet]
+        public IList<MostAnimalFromShelters> animalFromShelters()
+        {
+            return shelterLogic.animalFromShelters();
+        }
+
+        [Route("Dogshelters")]
+        [HttpGet]
+        public IList<DogShelter> DogShelters()
+        {
+            return shelterLogic.DogShelters();
+        }
+
+        [Route("Shelters")]
+        [HttpGet]
+        public IList<ShelterNames> SheltersName()
+        {
+            return shelterLogic.SheltersName();
+        }
+    }
 }
